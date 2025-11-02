4591d2d19bd029256adb9e5748edf33f9b5ac015
aparaschiveiadrian <adrian.aparaschivei04@gmail.com>
2025-11-02 23:30:48 +0200

    added terrain segments for for Straight/TurnLeft/TurnRight

---
+++ Assets/_Project/Scripts/Terrain/TerrainSegment.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class TerrainSegment : MonoBehaviour
+{
+    public SegmentType segmentType;
+
+    public Vector3 GetExitPosition()
+    {
+        Transform exitPoint = transform.Find("ExitPoint");
+        if (exitPoint != null)
+            return exitPoint.position;
+        return transform.position + transform.forward * 5f;
+    }
+
+    public Vector3 GetStartPosition()
+    {
+        Transform startPoint = transform.Find("StartPoint");
+        if (startPoint != null)
+            return startPoint.position;
+        //la inceputul segmentului
+        return transform.position - transform.forward * 5f;
+    }
+
+    public Quaternion GetExitRotation()
+    {
+        Transform exitPoint = transform.Find("ExitPoint");
+        if (exitPoint != null)
+            return exitPoint.rotation;
+        return transform.rotation;
+    }
+}
+
